c4e1a57ad0a3bc4194a7c26d3bc517ec75155dab
Dries <bester.dries@gmail.com>
2022-09-24 13:15:02 +0200

    created single post type and archive post type for events

--- functions.php
+++ functions.php
@@ -21,6 +21,10 @@
     //Im keeping this code here in order to remain a part of the git repo for the theme
         function university_post_types(){
             register_post_type('event', array(
+                'show_in_rest' => true,
+                'supports' => array('title','editor','excerpt'),
+                'rewrite' => array('slug' => 'events'),
+                'has_archive' => true,
                 'public' => true,
                 //'show_in_rest' => true,
                 'labels' => array(
