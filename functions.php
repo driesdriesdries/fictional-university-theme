44f62f5aa99540307ab47010829a1873db79f26a
Dries <bester.dries@gmail.com>
2022-09-29 16:23:09 +0200

    completed lesson 42 about registering and using custom image sizews on the front end

--- functions.php
+++ functions.php
@@ -14,6 +14,8 @@
         //register_nav_menu('footerLocationTwo','Footer Location Two');
         add_theme_support('title-tag');
         add_theme_support('post-thumbnails');
+        
+        //below registers custom image sizes.. the true value speaks to whether you want to crop
         add_image_size('professorLandscape', 400, 260, true);
         add_image_size('professorPortrait', 480, 650, true);
     }
